b1b1c96acad9382b415e6aafb888a5d8f70f7589
Valentin <johannsen@pflegonaut.de>
2019-08-08 09:42:45 +0200

    added addIngredients method

--- burger-app/src/containers (states)/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers (states)/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,15 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+// global constant-> capital letters
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.5,
+    meat: 1.5,
+    bacon: 1.0
+}
+
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -11,14 +20,40 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        // immutable update
+        // new js object with states
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+
+        // update price
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + newPrice;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render () {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls 
+                    // pass methods
+                    ingredientAdded={this.addIngredientHandler}
+                    
+                />
             </Aux>
         );
     }
