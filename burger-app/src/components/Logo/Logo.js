3278f925003bda2db5ee3a48d05329feade1b4b8
Valentin <johannsen@pflegonaut.de>
2019-08-09 23:23:56 +0200

    finished
    to be continued: sidedrawer

--- burger-app/src/components/Logo/Logo.js
+++ burger-app/src/components/Logo/Logo.js
@@ -1,17 +1,13 @@
+
 import React from 'react';
 
+import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from '../Logo/Logo.css';
 
-import burgerLogo from '../../assets/images/burger-logo.png';
+const logo = (props) => (
+    <div className={classes.Logo} style={{height: props.height}}>
+        <img src={burgerLogo} alt='MyBurger' />
+    </div>
+);
 
-
-const logo = (props) => {
-    return ( 
-        <div className={classes.Logo}>
-        {/* because src changed by webpack */}
-            <img src={burgerLogo} alt="MyBurger" />
-        </div>
-     );
-}
- 
 export default logo;
